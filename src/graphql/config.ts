e1b8a80d4e953892324235380ec680828c279445
Himank Barve <hbarve1592@gmail.com>
2023-09-20 15:52:53 +0530

    apollo graphql configured

---
+++ src/graphql/config.ts
@@ -0,0 +1,10 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+export const client = new ApolloClient({
+  uri: import.meta.env.VITE_GRAPHQL_API_URI,
+  cache: new InMemoryCache(),
+  credentials: 'include',
+  headers: {
+    authorization: import.meta.env.VITE_AUTH_TOKEN
+  }
+});
